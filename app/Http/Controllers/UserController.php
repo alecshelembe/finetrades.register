bfb19dbd0faf24c37c1a39d33bbfb5c87a6abe56
alecshelembe <alecshelembe@gmail.com>
2024-08-20 11:40:47 +0000

    updaed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,22 +16,24 @@ class UserController extends Controller
     }
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:2',
-        ]);
+        $data = $request->input();
+        dd($data);
+        // $request->validate([
+        //     'name' => 'required|string|max:255',
+        //     'email' => 'required|email|unique:users,email',
+        //     'password' => 'required|string|min:2',
+        // ]);
 
 
-        // Create the user
-        User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-        ]);
+        // // Create the user
+        // User::create([
+        //     'name' => $request->input('name'),
+        //     'email' => $request->input('email'),
+        //     'password' => Hash::make($request->input('password')),
+        // ]);
 
-        // Redirect to a route or another page
-        return redirect()->route('users.create')->with('success', 'User created successfully!');
+        // // Redirect to a route or another page
+        // return redirect()->route('users.create')->with('success', 'User created successfully!');
     }
 }
 
